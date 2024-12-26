695423cdda049597a87cea3b92298e181a31bb80
Fabricio S. Souza <fabriciopsgs@gmail.com>
2024-12-26 11:13:56 -0300

    Conversor de Volume pro Staging

--- program.c
+++ program.c
@@ -3,67 +3,79 @@
 #include <temperatureConverter.h>
 #include <powerConverter.h>
 #include <lengthConverter.h>
-#include <areaConverter.c>
-#include <areaConverter.h>
+#include <timeConverter.h>
 #include <volumeConverter.c>
 #include <volumeConverter.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <speedConverter.h>
 
-void GetHeader(){
+void GetHeader()
+{
     printf("\n\n");
     printf(">EmbarcaTech - Primeiro Projeto\n");
     printf(">Grupo4\n");
     printf(">Subgrupo 7\n\n");
 }
 
-void GetFooter(){
+void GetFooter()
+{
     printf("\nAté mais...\n\n");
 }
 
-void GetMenu(){
-    int menuController; //Controlador do menu
+void GetMenu()
+{
+    int menuController; // Controlador do menu
     double inputTemp;
-    CObject object;
+    //CObject object;
 
-    do {
+    do
+    {
         printf("\nEscolha uma opção de conversão:\n");
         printf("1. Unidades de comprimento\n");
         printf("2. Unidades de potência elétrica\n");
         printf("3. Conversão de Celsius para Fahrenheit\n");
         printf("4. Conversão de Celsius para Kelvin\n");
-        printf("9 - Conversão de volume\n");
+        printf("6. Conversão de velocidade\n");
+        printf("7. Conversao de tempo\n");
+        printf("9. Conversão de volume\n");
         printf("0. Sair\n");
 
         scanf("%d", &menuController);
 
-        switch(menuController){
-            case 1:
-                LengthConverter();
-                break;
-            case 2:
-                PowerConverter();
-                break;
-            case 3:
-                printf("Digite a temperatura em Celsius: ");
-                scanf("%lf", &inputTemp);
-                printf("Temperatura em Fahrenheit: %lf\n", CelsiusToFahrenheit(inputTemp));
-                break;
-            case 4:
-                printf("Digite a temperatura em Celsius: ");
-                scanf("%lf", &inputTemp);
-                printf("Temperatura em Kelvin: %lf\n", CelsiusToKelvin(inputTemp));
-                break;
-            case 5:
-                break;
-            case 9:{
-                volumeConverter();
-                break;
-            }
-            case 0:
-                printf("\nSaindo...\n");
-            default:
-                printf("\nOpção inválida, tente novamente.\n");
+        switch (menuController)
+        {
+        case 1:
+            LengthConverter();
+            break;
+        case 2:
+            PowerConverter();
+            break;
+        case 3:
+            printf("Digite a temperatura em Celsius: ");
+            scanf("%lf", &inputTemp);
+            printf("Temperatura em Fahrenheit: %lf\n", CelsiusToFahrenheit(inputTemp));
+            break;
+        case 4:
+            printf("Digite a temperatura em Celsius: ");
+            scanf("%lf", &inputTemp);
+            printf("Temperatura em Kelvin: %lf\n", CelsiusToKelvin(inputTemp));
+            break;
+        case 5:
+            break;
+        case 6:
+            speedConverter();
+            break;
+        case 7:
+            timeConverter();
+            break;
+        case 9:{
+            volumeConverter();
         }
-    } while(menuController != 0);
+        case 0:
+            printf("\nSaindo...\n");
+        default:
+            printf("\nOpção inválida, tente novamente.\n");
+        }
+    } while (menuController != 0);
 }
